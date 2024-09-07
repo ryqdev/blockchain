2e4d85b76fbb91a1371204f0dc758a91c486bb18
ryqdev <ykshang4119@gmail.com>
2024-09-08 00:12:35 +0800

    feat: very basic model

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type Block struct {
+	Data         string
+	Hash         string
+	PreviousHash string
+}
+
+type Blockchain struct {
+	Chain    []*Block
+	LastHash string
+}
+
+func (b *Block) Print() {
+	fmt.Printf("Previous Hash: %x\n", b.PreviousHash)
+	fmt.Printf("Data: %s\n", b.Data)
+	fmt.Printf("Hash: %x\n", b.Hash)
+	fmt.Println("---------------------------------------")
+}
+
+func CreateBlock(data, previousHash string) Block {
+	hash := sha256.Sum256([]byte(data + previousHash))
+	return Block{
+		data,
+		string(hash[:]),
+		string(previousHash),
+	}
+}
+
+func CreateChain() Blockchain {
+	return Blockchain{}
+}
+
+func (b *Blockchain) AddToChain(block *Block) {
+	b.Chain = append(b.Chain, block)
+	b.LastHash = block.Hash
+}
+
+func main() {
+	chain := CreateChain()
+	GenesisBlock := CreateBlock("Genesis", "")
+	chain.AddToChain(&GenesisBlock)
+
+	block1 := CreateBlock("Alice send 1 BTC to Bob", chain.LastHash)
+	chain.AddToChain(&block1)
+	for _, b := range chain.Chain {
+		b.Print()
+	}
+}
