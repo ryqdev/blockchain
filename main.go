1c902163752029a05f5c9d29fcf65655db87e6b1
ryqdev <ykshang4119@gmail.com>
2024-09-10 23:46:20 +0800

    feat: add pow

--- main.go
+++ main.go
@@ -3,7 +3,24 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"math/big"
 )
+
+const Difficulty = 5
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+	return &ProofOfWork{
+		b,
+		target,
+	}
+}
 
 type Block struct {
 	Data         string
